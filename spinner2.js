41e9355a47380c56e96367d18d176a21d03ef223
Sayantan Basu <sayantanbasu@live.in>
2021-06-01 18:51:24 -0400

    spiiner2.js

---
+++ spinner2.js
@@ -0,0 +1,13 @@
+const spin = () =>{
+  setTimeout(()=> process.stdout.write('\r|   '),100);
+  setTimeout(()=> process.stdout.write('\r/   '),400);
+  setTimeout(()=> process.stdout.write('\r-   '),700);
+  setTimeout(()=> process.stdout.write('\r\\   '),1000);
+  setTimeout(()=> process.stdout.write('\r|   '),1300);
+  setTimeout(()=> process.stdout.write('\r/   '),1600);
+  setTimeout(()=> process.stdout.write('\r-   '),1900);
+  setTimeout(()=> process.stdout.write('\r|   '),2100);
+}
+
+
+spin();
