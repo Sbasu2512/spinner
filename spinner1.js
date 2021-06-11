27abbee2349d70b8a990854e3619248cb1d25e47
Sayantan Basu <sayantanbasu@live.in>
2021-06-11 09:08:40 -0400

    finalcommit

--- spinner1.js
+++ spinner1.js
@@ -20,7 +20,6 @@ const spin = () => {
                    }, 700);
               
                    setTimeout(() => {
-                     // Need to escape the backslash since it's a special character.
                      process.stdout.write('\r\|   '); 
                    }, 900);
               
@@ -30,9 +29,12 @@ const spin = () => {
                    }, 1100);
                    
                    setTimeout(() => {
-                     // Need to escape the backslash since it's a special character.
                      process.stdout.write('\r\-   '); 
                    }, 1400);   
+
+                   setTimeout(() => {
+                     process.stdout.write('\r\\   '); 
+                   }, 1600);   
        }
 }
 
